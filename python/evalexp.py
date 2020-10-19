6d4d220bad6f8eadd620766c978328d7368db671
Cheruku Vamsi <65002479+Vamsi-027@users.noreply.github.com>
2020-10-19 12:45:06 +0530

    Delete evalexp.py

--- python/evalexp.py
+++
@@ -1,9 +0,0 @@
-# evaluates expression
-def eval_exp():
-    expr = input("Enter an expression")
-    try:
-        print("Result:", eval(expr))
-        print("-----------------")
-    except Exception as e:
-        print("Enter a valid expression")
-        print("ERROR:", str(e))
